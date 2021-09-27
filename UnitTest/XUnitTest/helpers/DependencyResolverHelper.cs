67c6951c0f8ff8cc5e83e87c7eac911117fcc880
Mahdi Rostami <MahdiRostami@Outlook.de>
2021-09-28 02:52:10 +0300

    Create xUnitTest project Add DependencyResolverHelper class for resolving DI Docker container OS changed to linux

---
+++ UnitTest/XUnitTest/helpers/DependencyResolverHelper.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Tasks;
+using Microsoft.AspNetCore;
+
+namespace XUnitTest.helpers
+{
+    public class DependencyResolverHelper
+    {
+        protected readonly IWebHost _webHost;
+        protected  IConfiguration _configuration => GetService<IConfiguration>();
+        protected IWebHostEnvironment _webHostEnvironment => GetService<IWebHostEnvironment>();
+        
+        public DependencyResolverHelper() {
+            _webHost = WebHost.CreateDefaultBuilder()
+                 .UseStartup<Startup>()
+                 .Build();
+           
+        }
+        
+        public T GetService<T>()
+        {
+            using (var serviceScope = _webHost.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+                try
+                {
+                    var scopedService = services.GetRequiredService<T>();
+                    return scopedService;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            };
+        }
+    }
+}
+
